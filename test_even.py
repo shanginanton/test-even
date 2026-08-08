ce6ea02e7d5827d744ebf6c5f64b16a4840502f0
shanginanton <shangin_anton@hotmail.com>
2026-08-08 17:48:53 +0300

    add CI workflow and tests

---
+++ test_even.py
@@ -0,0 +1,16 @@
+from even import is_even
+
+def test_is_even_with_even_number():
+	assert is_even(2) == True
+
+def test_is_even_with_odd_numger():
+	assert is_even(3) == False
+
+def test_is_even_with_zero():
+	assert is_even(0) == True
+
+def test_is_even_with_negative_even():
+	assert is_even(-2) == True
+
+def test_is_even_with_negative_odd():
+	assert is_even(-3) == False
